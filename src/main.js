938d14c3784551df7f05638408f38cf8f0a1dc8c
Andrej Sokolov <99323383+CyberPanda263@users.noreply.github.com>
2024-06-17 23:42:00 +0300

    f

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './js/question';
+import { getTheme } from './js/theme-module';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+getTheme();
+
+AOS.init();
+
+import './js/about-me';
+import './js/benefits';
+import './js/footer';
+import './js/header';
+import './js/hero';
+import './js/my-projects';
+import './js/question';
+import './js/reviews';
+import './js/theme-module';
+import './js/work-together';
